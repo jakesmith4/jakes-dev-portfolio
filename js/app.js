d7e2e40f0307e7fca67bb5819603ac965c57d61e
Jacob Smith <jakesmith.seo@gmail.com>
2021-08-08 19:32:00 -0500

    fixed service to have diffrent text content when clicked

--- js/app.js
+++ js/app.js
@@ -32,3 +32,52 @@ serviceLinks.forEach(function (link) {
     e.currentTarget.classList.toggle('click');
   });
 });
+
+const serviceLinkA = document.querySelector('.service-link-1');
+const serviceLinkB = document.querySelector('.service-link-2');
+const serviceLinkC = document.querySelector('.service-link-3');
+const serviceTextA = document.querySelector('.service-text-1');
+const serviceTextB = document.querySelector('.service-text-2');
+const serviceTextC = document.querySelector('.service-text-3');
+let clickDown = false;
+let clickDownB = false;
+let clickDownC = false;
+
+serviceLinkA.addEventListener('click', function () {
+  if (clickDown === false) {
+    clickDown = true;
+  } else {
+    clickDown = false;
+  }
+  if (clickDown) {
+    serviceTextA.textContent = `I work on every single project with pride, putting all my efforts into each of my projects, working on them until they are profected. Call me today to start your project`;
+  } else {
+    serviceTextA.textContent = `I am an expert in web development and I am certain you will be pleased with my work. I offer a 100% guarantee on all my work`;
+  }
+});
+
+serviceLinkB.addEventListener('click', function () {
+  if (clickDownB === false) {
+    clickDownB = true;
+  } else {
+    clickDownB = false;
+  }
+  if (clickDownB) {
+    serviceTextB.textContent = `Using the most modern techniques in web development, I will have your project done fast, and done the right way, with Beautful Images, Fonts and Colors of your choice!`;
+  } else {
+    serviceTextB.textContent = `My custom designs are eye popping and beautiful. Choose from custom colors of your choice and we will get to work on your beautiful website`;
+  }
+});
+
+serviceLinkC.addEventListener('click', function () {
+  if (clickDownC === false) {
+    clickDownC = true;
+  } else {
+    clickDownC = false;
+  }
+  if (clickDownC) {
+    serviceTextC.textContent = `All my projects will look good on mobile and desktop screens, as I design it to fit mobile screens first, then desktop next. No matter the screen size, you website will look great!`;
+  } else {
+    serviceTextC.textContent = `I do all my web development Optimised for mobile and desktop. When your site is complete it will be mobile and desktop friendly`;
+  }
+});
